d6bf685cd4f76a37887ea58411a464fda5e0226f
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-07-16 22:30:19 +0100

    add test for printing out statement header with ledger entry and write print method to pass test

--- lib/statement.rb
+++ lib/statement.rb
@@ -15,4 +15,14 @@ class Statement
     p "date || credit || debit || balance"
   end
 
+  def print
+    header = "date || credit || debit || balance"
+    ledger.each do |entry|
+      header << "\n "
+      entry.each do |k, _v|
+        header << "#{entry[k]} || "
+      end
+    end
+    header
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,7 +2,7 @@ require 'statement'
 require 'account'
 
 describe Statement do
-  date = Time.now
+  date = Time.new(2018, 7, 16).strftime("%d/%m/%Y")
 
   it "adds transactions to the ledger" do
     subject.add_transaction({ date: date, credit: 1000, debit: "", balance: 1000 })
@@ -13,7 +13,9 @@ describe Statement do
     expect(subject.header).to eq "date || credit || debit || balance"
   end
 
-  it "can" do
+  it "can print out the header and transaction" do
+    subject.add_transaction(date: date, credit: 1000, debit: "", balance: 1000)
 
+    expect(subject.print).to eq %Q(date || credit || debit || balance\n 16/07/2018 || 1000 ||  || 1000 || )
   end
 end
