591be700eb3d651b01b4b57a0a51232cbc33f395
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-07-16 11:23:16 +0100

    write first failing feature test for displaying account balance

---
+++ spec/statement_spec.rb
@@ -0,0 +1,9 @@
+require 'account'
+
+describe Account do
+  it 'can display a bank statement' do
+    account = Account.new
+    account.deposit(1000.00)
+    expect(account.balance).to eq 1000.00
+  end
+end
