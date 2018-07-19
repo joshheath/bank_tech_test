b2a4cbeddda99af20bdc8793342f97ad543fa6cc
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-07-19 17:25:50 +0100

    amend account_spec to work with reformatted time

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,19 +6,21 @@ describe Account do
     "date || credit || debit || balance\n 18/07/2018 || 750.00 ||  || 750.00"
   }
   let(:Account) { Account.new }
+  let(:Printer) { Printer.new }
+  let(:date) { Date.new }
 
   it 'displays a starting balance of zero' do
     expect(subject.balance).to eq 0
   end
 
   it 'can credit the balance' do
-    subject.credit(990.00)
+    subject.credit(date, 990.00)
     expect(subject.balance).to eq 990.00
   end
 
   it "can debit the balance" do
-    subject.credit(1000.00)
-    subject.debit(500.00)
+    subject.credit(date, 1000.00)
+    subject.debit(date, 500.00)
     expect(subject.balance).to eq 500.00
   end
 end
